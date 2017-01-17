fcd15bbe6cc4a4ebb51f13bb4c83eaf928bd91fa
ledan9286 <ledan9286@gmail.com>
2017-01-17 13:53:32 -0500

    Add files via upload

---
+++ User and Group Correlation.ps1
@@ -0,0 +1,57 @@
+﻿
+
+
+
+
+
+
+$array_username_group = @()
+
+$running_group = gwmi win32_group
+
+foreach ($organization in $running_group) {
+
+    
+    $temp_group=@{}
+    
+    $temp_group.Group = $organization.name 
+            
+        
+    $temp_group.User=@()
+
+    $a = (gwmi win32_groupuser | ? {$_.groupcomponent -match $organization.name} |% {[wmi]$_.partcomponent})
+
+                                    foreach ($b in $a){
+                                    
+                                        $temp_group.User += $b.name
+
+                                    } 
+
+                             
+
+    $array_username_group += $temp_group
+
+                           }
+
+
+$array_username_group |  ConvertTo-Json | Out-File array_username_group.json
+
+
+
+                         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
